352466ebf00c3f395ce17bec905423a6b267cd35
Brein <Brein_02_93@mail.ru>
2023-03-23 16:10:48 +0300

    Homework done

--- src/CyclesPartTwo.java
+++ src/CyclesPartTwo.java
@@ -1,6 +1,13 @@
 public class CyclesPartTwo {
     public static void main(String[] args) {
-    task8();
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
     }
     public static void task1() {
         System.out.println("Задача №1");
